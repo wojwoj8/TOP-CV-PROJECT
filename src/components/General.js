8004401be273a8c0a105be6d4be3faf7a0e8225f
wojwoj8 <wojwoj8@gmail.com>
2023-05-04 19:27:13 +0200

    added state for general

--- src/components/General.js
+++ src/components/General.js
@@ -6,24 +6,65 @@ class General extends Component{
         super(props);
         this.state = {
             showForm: false,
+            data: {
+                name: '',
+                surname: '',
+                email: '',
+            },
+            
         };
         this.onClickButt = this.onClickButt.bind(this);
     }
 
-    onClickButt(){
+    onClickButt = () => {
         this.setState((prevState) => ({
             showForm: !prevState.showForm,
         }))
     }
+    onEdit = (e) => {
+        const { name, value } = e.target;
+        this.setState((prevState) => ({
+          data: {
+            ...prevState.data,
+            [name]: value,
+          },
+        }));
+      }
+    onSub = (e) =>{
+        e.preventDefault();
+        // this.setState
+    }
     render(){
+        // const {name, surname, email} = this.state;
+
         const form = this.state.showForm ? (
-            <form>
+            <form onSubmit={this.onSub}>
                 <label htmlFor="name">Name: </label>
-                <input 
+                <input
+                name="name"
+                onChange={this.onEdit}
+                value={this.state.data.name} 
                 className="name">
                 </input>
+                <label htmlFor="surname">Surname: </label>
+                <input
+                onChange={this.onEdit} 
+                value={this.state.data.surname} 
+                name="surname"
+                className="surname">
+                </input>
+                <label htmlFor="email">Email: </label>
+                <input
+                onChange={this.onEdit}
+                name="email"
+                value={this.state.data.email} 
+                type="email" 
+                className="email">
+                </input>
+                <button type="submit">Submit</button>
             </form>
         ) :null;
+
         return(
             <div className="General">
                 <h1>General</h1>
