5083d63cb8a0235b845d89eb09eff6c05f716bd0
wojwoj8 <wojwoj8@gmail.com>
2023-05-06 11:19:13 +0200

    starting from 0

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 import './App.css';
 import React, { Component } from 'react';
-import Forms from './components/Forms';
-import Preview from './components/Preview';
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      formValues: {
-        name: '',
-        email: '',
-        surname: '',
-      },
-    };
-  }
-
-  handleFormChange = (name, value) => {
-    this.setState((prevState) => ({
-      formValues: {
-        ...prevState.formValues,
-        [name]: value,
-      },
-    }));
-  };
 
   render() {
   return (
     <div className="App">
-      <Forms></Forms>
-      <Preview formValues={this.state.formValues}></Preview>
+
     </div>
   );
 }
